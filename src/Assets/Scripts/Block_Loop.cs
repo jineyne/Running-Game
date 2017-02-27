0b238b022a2425c2438f7183f9a27184cad8a201
Yours <jineyne@gmail.com>
2017-02-27 22:36:28 +0900

    upload

---
+++ src/Assets/Scripts/Block_Loop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Block_Loop : MonoBehaviour
+{
+    public float Speed = 3;
+
+    public GameObject Block;
+    public GameObject A_ZONE;
+    public GameObject B_ZONE;
+
+    private void Move()
+    {
+        A_ZONE.transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        B_ZONE.transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        if (B_ZONE.transform.position.x <= 0)
+        {
+            Destroy(A_ZONE);
+            A_ZONE = B_ZONE;
+            Make();
+        }
+    }
+
+    private void Make()
+    {
+        /*B_ZONE = Instantiate(Block, new Vector3(30, -5, 0), transform.rotation) as GameObject;*/
+        B_ZONE = Instantiate(Block, new Vector3(A_ZONE.transform.position.x + 30, -5, 0), transform.rotation) as GameObject;
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Move();
+    }
+}
---
+++ src/Assets/Scripts/Scroll_Mapping.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Scroll_Mapping : MonoBehaviour
+{
+    public float ScrollSpeed = 0.5f;
+    private float Target_offset;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Target_offset += Time.deltaTime * ScrollSpeed;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(Target_offset, 0);
+    }
+}
